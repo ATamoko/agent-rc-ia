0f8c64401442721b94c16542bb14a64125bd0053
Abrah <abraham.tamoko@gmail.com>
2026-01-05 14:44:21 +0100

    Wire chat router into main app

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from app.api.chat import router as chat_router
 
 app = FastAPI(title="Responsable Relation Client IA")
+
+# Brancher les routes
+app.include_router(chat_router)
+
 
 @app.get("/")
 def healthcheck():
